f5e6e273b7bbbd730af016d03706b75ca80f7098
unknown <johannkubeja@gmail.com>
2023-05-04 06:41:24 -0700

    finishing touches

--- employeesDB/seed.sql
+++ employeesDB/seed.sql
@@ -1,40 +1,32 @@
-INSERT INTO Department (Department_name) 
-VALUES 
-("Branch Manager"), 
-("Finance"), 
-("Sales"), 
-("Software Engineer"), 
-("Legal"), 
-("Customer Service"), 
-("Reception"), 
-("Security");
+DROP DATABASE IF EXISTS employeesDB;
 
+CREATE DATABASE employeesDB;
 
-INSERT INTO Roles (title, salary, Department_id)
-VALUES 
-("Manager", "100000.00", 1), 
-("Accountant", "70000.00", 2), 
-("Salesperson", "50000.00",3), 
-("Engineer","90000.00",4), 
-("Human Resource","60000.00", 5), 
-("Reprensative", "40000.00", 6), 
-("Receptionist","35000.00", 7), 
-("Security Officer","30000.00",8);
+USE employeesDB;
 
-INSERT INTO Employee (first_name, last_name, roles_id, manager_id) 
-VALUES 
-("Dwight", "Schrute", 4, 1 ),
-("Michael", "Scott", "1", NULL), 
-("Jim", "Halpert", "4", "1"), 
-("Pam", "Beesly", "7", "1"), 
-("Stanley", "Hudson", "4","1"), 
-("Phyllis","Vance","4", "1"), 
-("Hank", "Tate", "8",NULL), 
-("Meridth", "Palmer", "3", "1"), 
-("Creed", "Bratton", "3", "1"), 
-("Ryan", "Howard", "3", "1"), 
-("Kelly", "Kapoor", "6", "1"), 
-("Toby", "Flenderson", "5", "1"), 
-("Oscar", "Martinez", "2", "1"), 
-("Kevin", "Malone", "2", "1"), 
-("Angela","Martin","2","1")
+CREATE TABLE Department (
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Roles (
+  title VARCHAR(30),
+  salary DECIMAL(10,2),
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES Department(id)
+);
+
+CREATE TABLE Employee (
+  first_name VARCHAR(45),
+  last_name VARCHAR(45),
+  role_id INT,
+  manager_id INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES Roles(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES Employee(id)
+
+);
