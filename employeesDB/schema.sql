cc5f786c33d49bc5a872e99006dcd1e9d406ffd2
unknown <johannkubeja@gmail.com>
2023-05-04 17:41:55 -0700

    Worked with tutor on formating schema and seed

--- employeesDB/schema.sql
+++ employeesDB/schema.sql
@@ -1,32 +1,34 @@
 DROP DATABASE IF EXISTS employeesDB;
-
 CREATE DATABASE employeesDB;
 
-USE employeesDB;
+USE employeesDB; 
 
-CREATE TABLE Department (
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+,  name VARCHAR(30) UNIQUE NOT NULL,
 );
 
-CREATE TABLE Roles (
-  title VARCHAR(30),
-  salary DECIMAL(10,2),
-  department_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id)
-  REFERENCES Department(id)
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  INDEX department_index (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
 );
 
-CREATE TABLE Employee (
-  first_name VARCHAR(45),
-  last_name VARCHAR(45),
-  role_id INT,
-  manager_id INT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id)
-  REFERENCES Roles(id),
-  FOREIGN KEY (manager_id)
-  REFERENCES Employee(id)
-
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  role_id INT UNISIGNED NOT NULL,
+  INDEX role_ind (role_id)
+  CONSTRAINT fk_role FOREIGN KEY (role_id) ON DELETE CASCADE
+  manager_id INT UNISIGNED,
+  INDEX manager_ind (manager_id)
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id)
+  REFERENCES employee(id),
+  ON DELETE SET NULL
 );
